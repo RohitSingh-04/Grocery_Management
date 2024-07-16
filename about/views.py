8fef86794b33268c7ee77f8b8af343990eb39429
RohitSingh-04 <rsingh.py@gmail.com>
2024-07-16 16:38:31 +0530

    user friendly about us

--- about/views.py
+++ about/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def about_page(request):
-    data = {'dev': 'Rohit Singh', 'github':'', 'linkedin':''}
+    data = {'dev': 'Rohit Singh', 'github':'https://github.com/RohitSingh-04', 'linkedin':'https://www.linkedin.com/in/rohit-singh-a59407274/'}
     return render(request, 'about.html', {'data': data})
