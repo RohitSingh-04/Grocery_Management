756a94368265624540915dd1f4792d96ec4f4072
RohitSingh-04 <rsingh.py@gmail.com>
2024-07-16 09:34:25 +0530

    set up virtual environment

--- grocery/views.py
+++ grocery/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.db.models import Sum, F, Count
+from django.db.models import Sum, F
 from .forms import *
 
 def login_usr(request):
